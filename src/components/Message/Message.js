c408c24ae7ca2b8c1c2d8a0a58f2cbb0dae56ef8
Roshan R <roshanrajeev2001@gmail.com>
2022-11-06 06:06:44 +0530

    Message Fix

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -16,13 +16,15 @@ const Message = ({ message, closeMessage }) => {
 
     return (
         <>
-            <div className={styles.Message}>
-                <p className={`
-                        ${styles.Message__text}
-                        ${message.type == "error" ? styles.Message__error : ""}
-                        ${message.type == "success" ? styles.Message__success : ""}
-                    `}>{message.text}</p>
-                <button className={styles.Message__close} onClick={closeMessage}>X</button>
+            <div className={`
+                ${styles.Message}
+                ${message.type == "error" ? styles.Message__error : ""}
+                ${message.type == "success" ? styles.Message__success : ""}
+            `}>
+                <p className={styles.Message__text}>{message.text}</p>
+                <button className={styles.Message__close} onClick={closeMessage}><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg></button>
             </div>
         </>
     );

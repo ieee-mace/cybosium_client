407da4111fc4c1d40a8a74b5640ff34c591f76f9
Roshan R <roshanrajeev2001@gmail.com>
2022-11-16 06:00:04 +0530

    Counter Animation

--- src/pages/Home/Counter/Counter.js
+++ src/pages/Home/Counter/Counter.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styles from "./Counter.module.scss";
+import { CountUp } from 'countup.js'
 
 const Counter = (props) => {
+    useEffect(() => {
+        const countUp1 = new CountUp('CounterVal1', 10, { enableScrollSpy: true, scrollSpyOnce: true });
+        const countUp2 = new CountUp('CounterVal2', 20, { enableScrollSpy: true, scrollSpyOnce: true });
+        const countUp3 = new CountUp('CounterVal3', 500, { enableScrollSpy: true, scrollSpyOnce: true });
+    }, [])
+
     return (
         <div className={styles.Counter}>
             <div className={styles.Counter__item}>
                 <p className={styles.Counter__text}>Number of speakers</p>
-                <h1 className={styles.Counter__number}>10+</h1>
+                <h1 className={styles.Counter__number}><span id="CounterVal1">10</span>+</h1>
             </div>
             <div className={styles.Counter__item}>
                 <p className={styles.Counter__text}>Number of events</p>
-                <h1 className={styles.Counter__number}>20+</h1>
+                <h1 className={styles.Counter__number}><span id="CounterVal2">20</span>+</h1>
             </div>
             <div className={styles.Counter__item}>
                 <p className={styles.Counter__text}>Number of participants</p>
-                <h1 className={styles.Counter__number}>500+</h1>
+                <h1 className={styles.Counter__number}><span id="CounterVal3">500</span>+</h1>
             </div>
         </div>
     )
